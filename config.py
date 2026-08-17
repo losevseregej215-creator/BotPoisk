794df08ac71c6f70519a06f6da45f84e87ec6b0f
losevseregej215-creator <losevseregej215@gmail.com>
2026-08-17 12:58:14 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = "ВАШ_ТОКЕН_БОТА"
+BOT_TOKEN = "8965288366:AAGOlMeVi7LLCBt5bu0PRXADARho03WGfiA"
 
 # URL вашего сайта (без слеша в конце)
-SITE_URL = "https://ваш-сайт.ru"
+SITE_URL = "http://miovidni.online/"
 
 # Путь к папке с БД
 DB_PATH = "data/bot.db"
